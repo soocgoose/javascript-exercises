5c1c37219f809678e366e16127e44b0bf1395a14
Shane O. O'Connor <skyfoosooc@gmail.com>
2024-03-04 22:32:26 -0500

    Provide passing solution to fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(lower, upper) {
+	if (typeof lower != 'number' || typeof upper != 'number') {
+		return 'ERROR';
+	}
 
+	if (lower < 0 || upper < 0) {
+		return 'ERROR';
+	}
+
+	if (lower > upper) {
+		const temp = upper;
+		upper = lower;
+		lower = temp;
+	}
+
+	let sum = 0;
+	for (let i = lower; i <= upper; i++) {
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
