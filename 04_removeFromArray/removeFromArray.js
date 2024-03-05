d0bcd1362af77aaa5c8b90a83cf0fd73a5e5ca2c
Shane O. O'Connor <skyfoosooc@gmail.com>
2024-03-04 22:12:51 -0500

    Provide passing solution to fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+	for (const e of elements) {
+		let i = -1;
+		while ((i = array.indexOf(e)) > -1) {
+			array.splice(i, 1);
+		}
+	}
+	return array;
 };
 
 // Do not edit below this line
