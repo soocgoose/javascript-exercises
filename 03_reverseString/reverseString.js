24a4b1b465cddef0d9ee35138bbf0d5e9a1285e1
Shane O. O'Connor <skyfoosooc@gmail.com>
2024-03-04 21:10:48 -0500

    Provide passing solution to third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+	let chars = Array.from(string);
 
+	let result = '';
+	for (let i = chars.length - 1; i >= 0; i--) {
+		result += chars[i];
+	}
+	return result;
 };
 
 // Do not edit below this line
